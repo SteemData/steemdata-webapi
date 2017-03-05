0d39d3e2499985b440a98d843e3a735acbabdcf1
furion <_@furion.me>
2017-03-05 16:28:37 +0100

    [webapi] deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t steemdata-api .
-docker tag steemdata-api furion/steemdata-api
-docker push furion/steemdata-api
+docker build -t steemdata-webapi .
+docker tag steemdata-webapi furion/steemdata-webapi
+docker push furion/steemdata-webapi
--- webapi.py
+++ webapi.py
@@ -11,10 +11,6 @@ app = Eve(settings='settings.py')
 Bootstrap(app)
 app.register_blueprint(eve_docs, url_prefix='/docs')
 
-# # init mongodb
-# app.config['MONGO_URI'] = 'mongodb://steemit:steemit@mongo1.steemdata.com:27017/SteemData'
-# mongo = PyMongo(app)
-
 if __name__ == '__main__':
     app.run(host=os.getenv('FLASK_HOST', '127.0.0.1'),
             debug=not os.getenv('PRODUCTION', False))
